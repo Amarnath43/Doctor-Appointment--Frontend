3f777099dee4fd196fdbf0217a75b2bff45cf31e
Amarnath43 <amarnathgoshika@gmail.com>
2025-07-25 16:49:30 +0530

    updated env.const.js file

--- src/components/hospital/HospitalCard.jsx
+++ src/components/hospital/HospitalCard.jsx
@@ -9,13 +9,22 @@ const HospitalCard = ({ id, name, imageUrl, location, doctorCount }) => {
     <div className="bg-white rounded-2xl border border-gray-100 shadow-sm
      hover:shadow-md transition-all duration-200 p-5 text-center flex flex-col
      items-center w-full h-full">
-      
+
       {/* Image */}
       <img
-        src={imageUrl ? imageUrl : '/default-hospital.png'}
+        src={
+          imageUrl
+            ? `${import.meta.env.VITE_BACKEND_URL}/${imageUrl}`
+            : '/default-hospital.png'
+        }
+        onError={(e) => {
+          e.target.onerror = null; // prevent infinite loop
+          e.target.src = '/default-hospital.png';
+        }}
         alt={name}
-        className="w-70 h-40 rounded-md  object-cover border-2 border-blue-100 mb-3"
+        className="w-70 h-40 rounded-md object-cover border-2 border-blue-100 mb-3"
       />
+
 
       {/* Name */}
       <h3 className="text-lg font-semibold text-gray-800">{name}</h3>
@@ -29,7 +38,7 @@ const HospitalCard = ({ id, name, imageUrl, location, doctorCount }) => {
       {/* Doctor Count */}
       <div className="flex items-center text-sm text-gray-500 mt-1">
         <Users size={16} className="mr-1 text-gray-400" />
-        <span>{doctorCount} {doctorCount>1?"Doctors":"Doctor"}</span>
+        <span>{doctorCount} {doctorCount > 1 ? "Doctors" : "Doctor"}</span>
       </div>
 
       {/* Button */}
